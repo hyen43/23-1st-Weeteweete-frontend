b06d39f44fc45d7e64adcd1db260944187c12227
lnhyen43@gmail.com <lnhyen43@gmail.com>
2021-08-02 18:41:33 +0900

    초기세팅 수정

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/Login" component={Login} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Main extends React.Component {
+  render() {
+    return <>main</>;
+  }
+}
+
+export default Main;
