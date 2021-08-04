90c1ed521aee9164fea97a72565446eb8a833c5c
최호정 <choihojung@choehojeong-ui-MacBookAir.local>
2021-08-04 16:22:23 +0900

    MODIFY : 품절, 할인기능 추가

--- src/pages/Main/MainBestItem.js
+++ src/pages/Main/MainBestItem.js
@@ -13,10 +13,11 @@ class MainBestItem extends Component {
             {bestList &&
               bestList.map(el => {
                 return el.information.map(pro => {
-                  const { id, name, image, discount, price } = pro;
+                  const { id, name, image, discount, price, stock } = pro;
                   return (
                     <li key={id}>
                       <Link to="/">
+                        {stock === 0 && <div className="soldOut">SOLD OUT</div>}
                         <div className="thumb">
                           <div className="img">
                             <img alt={name} src={image[0]}></img>
@@ -32,13 +33,15 @@ class MainBestItem extends Component {
                             <span className="cost">
                               {Number(price).toLocaleString()} 원
                             </span>
-                            <span className="salePrice">
-                              할인 판매가 :
-                              {(
-                                Number(price) - Number(discount)
-                              ).toLocaleString()}
-                              원
-                            </span>
+                            {discount !== '0' && (
+                              <span className="salePrice">
+                                할인 판매가 :
+                                {(
+                                  Number(price) - Number(discount)
+                                ).toLocaleString()}
+                                원
+                              </span>
+                            )}
                           </div>
                         </div>
                       </Link>
