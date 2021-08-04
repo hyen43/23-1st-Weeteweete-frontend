16690f5f930f2eca5c83187ca576b48d328f9f05
user1 <user1@user1ui-MacBook-Air.local>
2021-08-04 15:33:02 +0900

    Modify : 회원가입 레이아웃 수정 2

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './Signup.scss';
+import SIGNUP_LIST from './SignupData.js';
 
 class Signup extends React.Component {
   render() {
@@ -15,69 +16,19 @@ class Signup extends React.Component {
         </div>
         <div className="signUpContainer">
           <form className="signUpForm">
-            <div className="signUpFormId">
-              <span className="signUpFormIndex">
-                아이디<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="text" />
-              <span>(영문소문자/숫자, 4~16자)</span>
-            </div>
-            <div className="signUpFormPw">
-              <span className="signUpFormIndex">
-                비밀번호<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="password" />
-              <span>
-                (영문 대소문자/숫자/특수문자 중 2가지 이상 조합, 10자~16자)
-              </span>
-            </div>
-            <div className="signUpFormRePw">
-              <span className="signUpFormIndex">
-                비밀번호 확인<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="password" />
-            </div>
-            <div className="signUpFormName">
-              <span className="signUpFormIndex">
-                이름<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="text" />
-            </div>
-            <div className="signUpFormAddress">
-              <span className="signUpFormIndex">
-                주소<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="text" />
-            </div>
-            <div className="signUpFormPhone">
-              <span className="signUpFormIndex">
-                전화번호<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="text" />
-            </div>
-            <div className="signUpFormMail">
-              <span className="signUpFormIndex">
-                이메일<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="text" />
-            </div>
-            <div>
-              <span className="signUpFormIndex">
-                이메일 수신여부<span className="signUpEssentialMark">*</span>
-              </span>
-              <input type="checkbox" />
-              <span>
-                쇼핑몰에서 제공하는 유익한 이벤트 소식을 이메일로 받으실 수
-                있습니다.
-              </span>
-            </div>
-            <div>
-              <span className="signUpFormName">* 주의 *</span>
-              <span>
-                이름/휴대전화/이메일은 추후 비밀번호 찾기에 필요하니 꼭 정확하게
-                입력해 주세요 :D
-              </span>
-            </div>
+            {SIGNUP_LIST.map(list => {
+              const { key, index, type, explain, name, className } = list;
+              return (
+                <div className="signUpFormBox" key={key}>
+                  <span className="signUpFormIndex">
+                    {index}
+                    <span className="signUpEssentialMark">*</span>
+                  </span>
+                  <input type={type} name={name} className={className} />
+                  <span>{explain}</span>
+                </div>
+              );
+            })}
           </form>
         </div>
         <div className="signUpBtnList">
