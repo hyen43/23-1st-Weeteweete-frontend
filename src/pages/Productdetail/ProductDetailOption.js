361700cd25f3ba3f110df456566b363e81f1fb1e
lnhyen43@gmail.com <lnhyen43@gmail.com>
2021-08-05 21:48:36 +0900

    Modify: 필요없는 내용 수정

--- src/pages/Productdetail/ProductDetailOption.js
+++ src/pages/Productdetail/ProductDetailOption.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ProductDetailOption extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: '',
-    };
-  }
-
   render() {
     const { discount_price } = this.props.detailData;
 
