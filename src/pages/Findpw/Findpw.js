61ebe057f8dffbdcd30319b9d6dcd19560e86ae8
user1 <user1@user1ui-MacBook-Air.local>
2021-08-07 00:26:28 +0900

    Modify : 비밀번호 찾기 페이지 레이아웃 수정

--- src/pages/Findpw/Findpw.js
+++ src/pages/Findpw/Findpw.js
@@ -1,54 +1,42 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Findpw.scss';
+import FINDPW_LIST from './FindpwData.js';
 
 class Findpw extends React.Component {
   render() {
     return (
-      <div className="findpw">
+      <main className="findpw">
         <h2 className="findpwTitle">FIND PW</h2>
         <div className="findpwContainer">
           <div className="findpwMethod">
-            <input type="radio" name="findpwWhat" />
-            이메일로 찾기
-            <input type="radio" name="findpwWhat" />
-            휴대폰 번호로 찾기
+            <span className="selectfindWay">
+              가입시 사용한 아이디, 이름, 이메일이 필요합니다.
+            </span>
           </div>
-          <form>
-            <table className="findpwTable">
-              <tbody>
-                <tr>
-                  <th scope="row">아이디</th>
-                  <td>
-                    <input type="text" className="findpwIdInput" />
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">이름</th>
-                  <td>
-                    <input type="text" className="findpwNameInput" />
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">이메일</th>
-                  <td>
-                    <input type="text" className="findpwMailInput" />
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">휴대폰 번호</th>
-                  <td>
-                    <input type="text" className="findpwPhoneInput" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
+          <div className="findpwForm">
+            <form>
+              {FINDPW_LIST.map(list => {
+                const { key, index, type, name } = list;
+                return (
+                  <div className="findpwName" key={key}>
+                    <span className="findpwFormName">{index}</span>
+                    <input
+                      type={type}
+                      name={name}
+                      className="findpwInput"
+                    ></input>
+                  </div>
+                );
+              })}
+            </form>
+          </div>
+
           <button className="findpwBtn">확인</button>
         </div>
-      </div>
+      </main>
     );
   }
 }
 
-export default Findpw;
+export default withRouter(Findpw);
---
+++ src/pages/Findpw/FindpwData.js
@@ -0,0 +1,22 @@
+const FINDPW_LIST = [
+  {
+    key: 1,
+    index: '아이디',
+    type: 'text',
+    name: 'account',
+  },
+  {
+    key: 2,
+    index: '이름',
+    type: 'text',
+    name: 'name',
+  },
+  {
+    key: 3,
+    index: '이메일',
+    type: 'text',
+    name: 'email',
+  },
+];
+
+export default FINDPW_LIST;
