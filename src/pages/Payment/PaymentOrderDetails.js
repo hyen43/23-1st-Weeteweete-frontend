c964e0e48ef9f165271fce9d99a7e116c33b5986
user1 <user1@user1ui-MacBook-Air.local>
2021-08-11 01:24:59 +0900

    Add : 구매페이지 주문상품목록 컴포넌트 기능

--- src/pages/Payment/PaymentOrderDetails.js
+++ src/pages/Payment/PaymentOrderDetails.js
@@ -2,7 +2,35 @@ import React from 'react';
 import './PaymentOrderDetails.scss';
 
 class PaymentOrderDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paymentList: [],
+      productPrice: 0,
+      discountPrice: 0,
+      totalPrice: 0,
+    };
+  }
+
+  componentDidMount() {
+    fetch('/data/PaymentProductData.json', {})
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          paymentList: data.data,
+        });
+      });
+  }
+
   render() {
+    const totalPrice = this.state.paymentList.map(el => {
+      return (this.state.totalPrice +=
+        Number(el.information[0].product_discount) * Number(el.quantity));
+    });
+    const productPrice = this.state.paymentList.map(el => {
+      return (this.state.productPrice +=
+        Number(el.information[0].product_price) * Number(el.quantity));
+    });
     return (
       <main className="paymentOrderDetails">
         <section className="orderProductList">
@@ -16,32 +44,75 @@ class PaymentOrderDetails extends React.Component {
             <h3>국내배송상품 주문내역</h3>
           </header>
           <table className="orderDetailsTable">
-            {ORDER_DETAILS_LIST.map(list => {
-              const { key, content } = list;
-              return (
-                <th className="tableStyle" key={key}>
-                  {content}
-                </th>
-              );
-            })}
-            <tr>
-              <td className="tableline">{'img'}</td>
-              <td className="tableline">{'product'}</td>
-              <th className="tableline">{'price'}</th>
-              <th className="tableline">{'quantity'}</th>
-              <th className="tableline">{'delivery'}</th>
-              <th className="tableline">{'delivery fee'}</th>
-              <th className="tableline">{'total'}</th>
-            </tr>
-            <th colspan="9" className="totalPrice">
-              <strong>기본배송</strong> 상품구매금액 {''}+ 배송비 {''} -
-              상품할인금액 {''} = 합계:
-              <span>
-                {''}
-                <strong>원</strong>
-              </span>
-            </th>
+            <thead>
+              <tr>
+                {ORDER_DETAILS_LIST.map(list => {
+                  const { key, content } = list;
+                  return (
+                    <th className="tableStyle" key={key}>
+                      {content}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.paymentList.map(el => {
+                return (
+                  <tr key={el.order_number}>
+                    <td className="tableline">
+                      {el.information[0].product_image}
+                    </td>
+                    <td className="tableline">
+                      {el.information[0].product_name}
+                    </td>
+                    <td className="tableline">
+                      {el.information[0].product_price}
+                    </td>
+                    <td className="tableline">{el.quantity}</td>
+                    <td className="tableline">
+                      {Number(el.information[0].product_discount) *
+                        Number(el.quantity) >=
+                      30000
+                        ? 0
+                        : 2500}
+                    </td>
+                    <td className="tableline">
+                      {Number(el.information[0].product_discount) *
+                        Number(el.quantity)}
+                    </td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td colSpan="9" className="totalPrice">
+                  상품구매금액 {productPrice[productPrice.length - 1]}{' '}
+                  <span>
+                    <strong> 원</strong>
+                  </span>
+                  - 상품할인금액{' '}
+                  {productPrice[productPrice.length - 1] -
+                    totalPrice[totalPrice.length - 1]}{' '}
+                  <span>
+                    <strong> 원</strong>
+                  </span>
+                  + 배송비{' '}
+                  {totalPrice[totalPrice.length - 1] < 30000 ? 2500 : 0}{' '}
+                  <span>
+                    <strong> 원</strong>
+                  </span>
+                  =
+                  {totalPrice[totalPrice.length - 1] < 30000
+                    ? totalPrice[totalPrice.length - 1] + 2500
+                    : totalPrice[totalPrice.length - 1]}
+                  <span>
+                    <strong> 원</strong>
+                  </span>
+                </td>
+              </tr>
+            </tbody>
           </table>
+
           <ul>
             <li>
               <i className="faExclamation">!</i>
@@ -59,7 +130,6 @@ const ORDER_DETAILS_LIST = [
   { key: 1, content: '상품정보' },
   { key: 2, content: '판매가' },
   { key: 3, content: '수량' },
-  { key: 4, content: '배송구분' },
   { key: 5, content: '배송비' },
   { key: 6, content: '합계' },
 ];
