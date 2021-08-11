15d6b6f69a6ed45cc7b9504bd274d1519c76dcf8
user1 <user1@user1ui-MacBook-Air.local>
2021-08-11 15:48:52 +0900

    Modify : 회원가입 유효성 검사 로직 수정

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import SIGNUP_LIST from './SignupData.js';
 import './Signup.scss';
-import { validationFunction } from '../../utils/validation.js';
+import { validationFunction } from '../../utils/validation';
 
 class Signup extends React.Component {
   constructor(props) {
@@ -11,11 +11,11 @@ class Signup extends React.Component {
       userInfo: {
         account: '',
         password: '',
-        name: '',
-        address: '',
-        phone_number: '',
         email: '',
       },
+      name: '',
+      address: '',
+      phone_number: '',
       repassword: '',
     };
   }
@@ -23,57 +23,45 @@ class Signup extends React.Component {
   handleInput = e => {
     const { name, value } = e.target;
     this.setState({
-      userInfo: { ...this.state.userInfo, [name]: value },
+      userInfo: {
+        ...this.state.userInfo,
+        [name]: value,
+      },
+      [name]: value,
     });
   };
 
-  isAllValid = property => {
-    const alertMessage = {
-      account: '아이디가 유효하지 않습니다!',
-      password: '비밀번호가 유효하지 않습니다!',
-      email: '이메일이 유효하지 않습니다!',
-    };
+  testValidation = () => {
+    const validInfo = Object.entries(this.state.userInfo);
 
-    // for - in 으로 validationFunction 의 key 값을 가져온다.
-    for (property in validationFunction) {
-      // 작동시 key에 맞는 함수 호출
-      const validationKey = validationFunction[property];
-
-      // 작동시 key에 맞는 value 값 호출
-      const validationValue = this.state.userInfo[property];
-
-      // 함수의 value 값이 유효성을 만족하지 않으면 실행
-      if (!validationKey(validationValue)) {
-        console.log(!validationKey(validationValue));
-        return console.log(alertMessage[property]);
-      }
-    }
+    return validInfo.every(info => validationFunction[info[0]](info[1]));
   };
 
   signupTest = () => {
-    const { account, password, name, address, phone_number, email } =
-      this.state;
-    fetch('http://10.58.2.84:8000/users/singup', {
-      method: 'POST',
-      // userInfo 객체 자체를 POST 보내려고 하면 통신 연결 안됨
-      body: JSON.stringify({
-        account,
-        password,
-        name,
-        address,
-        phone_number,
-        email,
-      }),
-    })
-      .then(res => res.json())
-      .then(result => {
-        if (result.MESSAGE === 'SUCCESS') {
-          alert('회원가입 성공! 로그인 페이지로 이동합니다!');
-          this.props.history.push('/login');
-        } else {
-          alert('회원가입 실패!');
-        }
-      });
+    if (this.testValidation()) {
+      fetch('http://10.58.5.153:8000/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+          account: this.state.userInfo.account,
+          password: this.state.userInfo.password,
+          name: this.state.name,
+          address: this.state.address,
+          phone_number: this.state.phone_number,
+          email: this.state.userInfo.email,
+        }),
+      })
+        .then(res => res.json())
+        .then(result => {
+          if (result.MESSAGE === 'SUCCESS') {
+            alert('회원가입 성공! 로그인 페이지로 이동합니다!');
+            this.props.history.push('/login');
+          } else {
+            alert('회원가입 실패!');
+          }
+        });
+    } else {
+      return alert('회원가입 조건을 지켜주세요!');
+    }
   };
 
   render() {
@@ -102,13 +90,6 @@ class Signup extends React.Component {
                       name={name}
                       className={className}
                       onChange={this.handleInput}
-                      onBlur={
-                        name === 'account' ||
-                        name === 'password' ||
-                        name === 'email'
-                          ? this.isAllValid()
-                          : null
-                      }
                     />
                     <span>{explain}</span>
                   </div>
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,16 +1,16 @@
-const validateAccount = value => {
+const validateAccount = account => {
   const accountCheck = /^[a-z0-9]{10,20}$/;
-  return accountCheck.test(value);
+  return accountCheck.test(account);
 };
 
-const validatePassword = value => {
+const validatePassword = password => {
   const passwordCheck = /^[0-9]{8,20}$/;
-  return passwordCheck.test(value);
+  return passwordCheck.test(password);
 };
 
-const validateEmail = value => {
+const validateEmail = email => {
   const emailCheck = /^[0-9a-zA-Z]+@[0-9a-zA-Z]*\.[a-zA-Z]{2,3}$/;
-  return emailCheck.test(value);
+  return emailCheck.test(email);
 };
 
 export const validationFunction = {
