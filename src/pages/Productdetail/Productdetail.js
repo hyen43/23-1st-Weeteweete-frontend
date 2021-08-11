5ea2046cd9caf3a37f579ea175242260d547fd0f
lnhyen43@gmail.com <lnhyen43@gmail.com>
2021-08-11 20:44:37 +0900

    ADD: modal

--- src/pages/Productdetail/Productdetail.js
+++ src/pages/Productdetail/Productdetail.js
@@ -5,6 +5,7 @@ import ProductDetailInfo from './productDetailInfo';
 import ProductDetailOption from './ProductDetailOption';
 import ProductDetailButtons from './ProductDetailButtons';
 import Review from '../Review/Review';
+import Modal from './Modal';
 import { BASE_URL } from '../../config.js';
 import './Productdetail.scss';
 
@@ -13,8 +14,16 @@ class Productdetail extends React.Component {
     super(props);
     this.state = {
       detailData: {},
+      modalOpen: false,
     };
   }
+
+  openModal = () => {
+    this.setState({ modalOpen: true });
+  };
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
 
   componentDidMount() {
     //console.log(this.props.match.params.id)
@@ -33,6 +42,7 @@ class Productdetail extends React.Component {
 
   render() {
     const { detailData } = this.state;
+    const { openModal } = this;
     return (
       <main className="productDetailPage">
         <div className="productDetail">
@@ -40,7 +50,8 @@ class Productdetail extends React.Component {
           <section>
             <ProductDetailInfo detailData={detailData} />
             <ProductDetailOption detailData={detailData} />
-            <ProductDetailButtons />
+            <ProductDetailButtons openModal={openModal} />
+            <Modal open={this.state.modalOpen} close={this.closeModal} />
           </section>
         </div>
         <Review paramsId={this.props.match.params.id} />
